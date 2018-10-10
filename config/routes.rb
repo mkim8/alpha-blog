266345914bf489a84d9a4e953511bf0a81f6d3ed
Maria Kim <maria.kim@gmail.com>
2018-10-10 20:01:15 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   
   resources :articles
+  
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
