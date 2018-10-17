000287a2457924317a60a7f1973e0480205ca620
Maria Kim <maria.kim@gmail.com>
2018-10-17 21:18:16 +0000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
       @articles = Article.paginate(page: params[:page], per_page:5)
@@ -46,8 +48,8 @@ class ArticlesController < ApplicationController
     def set_article
       @article = Article.find(params[:id])
     end
+  
     def article_params
       params.require(:article).permit(:title, :description)
     end
-  
 end
